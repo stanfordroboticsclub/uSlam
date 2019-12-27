ab429c68159fc3b7673833b58760614061985446
Michal Adamkiewicz <mikh3x4@gmail.com>
2019-12-27 12:09:50 +0100

    new slams

---
+++ dp_slam.py
@@ -0,0 +1,40 @@
+
+
+
+
+
+class Particle:
+
+    def __init__(self):
+        self.parent = None
+        self.children = []
+
+        self.onodes = []
+
+    def prune(self):
+        pass
+
+    def colapse(self):
+        pass
+
+
+class ONode:
+
+    def __init__(self):
+        self.passes = 0
+        self.hits = 0
+
+        self.ancestor = 
+        self.anode = 
+
+class Grid:
+
+    def __init__(self):
+        pass
+        self.grid = []
+
+
+
+class SLAM:
+    def __init__(self):
+        pass
