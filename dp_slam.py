10f1773c507c75cb6041937ff09bc7b79977c088
Michal Adamkiewicz <mikh3x4@gmail.com>
2019-12-27 20:24:13 +0100

    added pose calcualtions

--- dp_slam.py
+++ dp_slam.py
@@ -4,6 +4,7 @@ import numpy as np
 
 
 class Pose():
+    lidar_offset_forward = 0
     def __init__(self):
         self.pose = np.array([0,0,0])
 
@@ -22,8 +23,26 @@ class Pose():
     @a.setter
     def a(self, value): self.pose[2] = value 
 
-    def move(dpose):
-        pass
+    @property
+    def loc(self):
+        return (self.x, self.y)
+
+    def move(self, dpose):
+        v_forward, v_right, v_th = dpose
+        eff_a = self.a + v_th/2
+
+        # np.rot_matrix( eff_a) @ dpose[:1]
+
+        delta_x = (v_forward * np.cos(eff_a) ) - v_right * math.sin(eff_a))
+        delta_y = (v_forward * np.sin(eff_a) ) + v_right * math.cos(eff_a))
+
+        self.x += delta_x;
+        self.y += delta_y;
+        self.th += v_th;
+
+    def laser(self, dist, angle):
+        return (self.x + dist * np.cos(angle + self.a) + self.lidar_offset_forward * np.cos(self.a), 
+                self.y + dist * np.sin(angle + self.a) + self.lidar_offset_forward * np.sin(self.a))
 
 
 class Particle:
@@ -32,17 +51,16 @@ class Particle:
         self.parent = parent
         self.children = []
 
-        self.onodes = []
+        self.onodes = {}
         self.pose = parent.pose.copy()
 
     def prune(self):
         pass
 
-    def colapse(self):
-        pass
 
     def update_odometry(self, delta_pose):
-        pass
+        self.pose.move(delta_pose)
+        self.pose.move([0,0,0])
 
     def update_lidar(self, scan):
         pass
@@ -54,13 +72,24 @@ class Particle:
         child = Particle(self)
         child.pose.move([0,0,0])
         self.children.append(child)
+
+    def die(self):
+        if len(self.children) == 1:
+            pass
+            # self = self.children[0]
+
+        
+    def grid(self, loc):
+        particle = self
+        while loc not in particle.onodes.keys():
+            particle = particle.parent
+            if particle is None:
+                return None
+        onode = particle.onodes[loc]
+        return onode.hits/onode.passes
         
 
-    def grid(self,grid):
-        grid[self.pose.
-
 class ONode:
-
     def __init__(self, anode, ancestor):
         self.passes = ancestor.passes
         self.hits = ancestor.hits
@@ -74,21 +103,53 @@ class Grid:
     GRID_SIZE = 400
 
     def __init__(self):
-        pass
-        self.grid = []
+        grid_squares = self.GRID_SIZE // self.MM_per_GRID
+        self.grid = [[[] for _ in range(grid_squares)] for _ in range(grid_squares)]
+
+
+    def __getitem__(self, key):
+        x, y = key
+        return self.grid[x//self.MM_per_GRID][y//self.MM_per_GRID]
+
+    def __setitem__(self, key, value):
+        x, y = key
+        self.grid[x//self.MM_per_GRID][y//self.MM_per_GRID].append(value)
 
 
 
 
 
 class SLAM:
+    PARTICLE_NUM = 100
     def __init__(self):
         self.odom =  Subscriber()
         self.lidar = Subscriber()
 
+        self.particle_tree = Particle()
+        self.particle_list = [self.particle_tree]
 
-        self.particle_tree = 
-        pass
+    def populate_particles(self):
+        while len(self.particle_list) < self.PARTICLE_NUM:
+            random.choice(self.particle_list).spawn_child()
+
+    def update_odometry(self, odometry_msg):
+        
+        delta_pose = np.array(odometry_msg)
+        for particle in self.particle_list:
+            particle.update_odometry(delta_pose)
+
+    def update(self):
+        self.update_odometry()
+        
+        self.calculate_posteriors()
+
+        self.update_maps()
+
+        self.populate_particles()
+
+
+
+
 
 
 
