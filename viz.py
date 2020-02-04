b08ac5b4548323d3e5ba2cf6125b84c2e2029359
Michal Adamkiewicz <mikh3x4@gmail.com>
2020-02-03 20:50:07 -0800

    moving away from hough

--- viz.py
+++ viz.py
@@ -35,25 +35,41 @@ def find_landmarks(scan):
         xs = []
         ys = []
 
-        img = np.zeros((500,500), dtype=np.uint8)
+        img = np.zeros((600,600), dtype=np.uint8)
 
         for _, angle, dist in scan:
             a = math.radians(angle)
-            x = int(250 + math.sin(a) * dist/10)
-            y = int(250 - math.cos(a) * dist/10)
-            img[x,y] = 255
+            x = int(350 + math.sin(a) * dist/10)
+            y = int(350 - math.cos(a) * dist/10)
+            try:
+                img[x,y] = 255
+            except IndexError:
+                pass
 
 
-        blur = 255 - img #cv2.GaussianBlur(img,(3,3),0)
+        # blur = 255 - img #cv2.GaussianBlur(img,(3,3),0)
+        blur = 255 - 10*cv2.GaussianBlur(img,(7,7),0)
+        blur[blur>255] = 255
+        blur[blur<0] = 0
 
-        lines = cv2.HoughLinesP(blur,1,np.pi/180,100,20,10)
-        print("lines",lines)
+        # lines = cv2.HoughLinesP(blur,1,np.pi/180,100,100,100)
+        lines = cv2.HoughLines(blur,1,np.pi/180,10)
+        # print("lines",lines)
 
         if lines is None:
-            # cv2.imshow("frame",blur)
+            cv2.imshow("frame",blur)
             return 
 
-        for x1,y1,x2,y2 in lines[0]:
+        print(len(lines[0]))
+        for rho,theta in lines[0]:
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a*rho
+            y0 = b*rho
+            x1 = int(x0 + 1000*(-b))
+            y1 = int(y0 + 1000*(a))
+            x2 = int(x0 - 1000*(-b))
+            y2 = int(y0 - 1000*(a))
             blur = cv2.line(blur,(x1,y1),(x2,y2),(0),4)
 
         cv2.imshow("frame",blur)
