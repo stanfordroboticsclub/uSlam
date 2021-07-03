259fb3815f363042d7cb9d2b7f8d2042be5fe5fa
Michal Adamkiewicz <mikh3x4@gmail.com>
2021-07-03 02:47:15 -0700

    some work??

--- pose_graph.py
+++ pose_graph.py
@@ -20,10 +20,29 @@ class PoseGraph:
 
         self.graph = nx.Graph()
 
+        self.node_count = 0
+
+    def initialize(self, point_cloud):
+        pass
+        self.graph.
+
     def add_node(self, node, links):
         pass
 
-    def get_nearby_poses(self, location, distance):
+    def get_nearby_poses(self, location, max_distance):
+        robot = location.get_components()[1]
+
+        out = []
+        for node,data in self.graph.nodes(data=True):
+            loc = data['pose'].get_components()[1]
+            distance = np.linalg.norm(robot - loc)
+            if distance < max_distance:
+                out.append( (distance, node,) )
+
+        return sorted(out)
+
+
+    def optimize(self):
         pass
 
 
